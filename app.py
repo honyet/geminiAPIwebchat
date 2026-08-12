39cf3edd4997ec42cd3778ee930db0b4db54ddad
honyet <hong9906@163.com>
2026-08-12 11:36:13 +0800

    修改默认

--- app.py
+++ app.py
@@ -31,10 +31,10 @@ with st.sidebar:
 
     # 模型选择
     model_map = {
-        "Gemini 3.1 Pro": "gemini-3.1-pro-preview",
-        "Gemini 3 Pro": "gemini-3-pro-preview",
         "Gemini-3.5-flash-lite": "gemini-3.5-flash-lite",
         "Gemini-3.6-flash": "gemini-3.6-flash",
+        "Gemini 3.1 Pro": "gemini-3.1-pro-preview",
+        "Gemini 3 Pro": "gemini-3-pro-preview",
     }
 
     selected_label = st.selectbox(
